3488ac891f9375ccb24a8dea70e5a5c4a67089a6
PrashantSharma0512 <prashantsharma0512@gmail.com>
2025-06-01 09:35:52 +0530

    add cors for all

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const mongoConnect = require('./connection');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://coderhaveli.vercel.app/'], // Add your domain here
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
