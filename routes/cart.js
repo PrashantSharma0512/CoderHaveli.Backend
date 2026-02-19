0307d3516c33da0687fc97cfbb6d7c9a97193872
PrashantSharma0512 <prashantsharma0512@gmail.com>
2026-02-19 19:34:29 +0530

    make changes in the cart route

--- routes/cart.js
+++ routes/cart.js
@@ -5,7 +5,7 @@ const Authenticate = require('../middlewares/Auth')
 router.post('/remove', Authenticate, cartController.removeItem);
 router.get('/', Authenticate, cartController.getCartItems);
 router.post('/add', Authenticate, cartController.addItems);
-router.post('/clear', Authenticate, cartController.clearCart);
+router.delete('/clear', Authenticate, cartController.clearCart);
 router.get('/user-cart-count', Authenticate, cartController.getUserCartCount);
 
 module.exports = router
